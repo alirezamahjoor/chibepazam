35ead8438399e84a8f7a5fda52f40fb0525bc6e0
alireza.nasrollahi.mahjoor <alireza.mahjoor@outlook.com>
2017-12-21 21:49:30 +0330

    changes for admin

--- index.php
+++ index.php
@@ -14,21 +14,49 @@ include 'db.php';
 <body  dir="rtl">
 
 
-    <h1>مواد اولیه</h1>
+<div align="center"><h1 >چی بپزم؟</h1>
+</div>
+
+
+
+<table>
+    <tr>
+        <td align="center" >
+
+    <h1 style="background-color:lightgreen">مواد اولیه</h1>
+        </td >
+    </tr>
+    <tr>
+        <td>
+            <div class="scroll"><form action="#" method="post">
 <?php
-$primary_query=mysqli_query($connect,"SELECT name FROM t_primary");
+$primary_query=mysqli_query($connect,"SELECT id,name FROM t_primary");
 $primary=mysqli_fetch_all($primary_query,MYSQLI_ASSOC);
 foreach($primary as $item){
 ?>
-
     <label class="container"><?php echo $item['name']; ?>
-    <input type="checkbox" >
+    <input type="checkbox" name="<?php echo $item['id'];?>" >
     <span class="checkmark"></span>
 
     </label>
 
 <?php } ?>
+                </form>
+            </div>
+
+        </td>
 
 
+
+    </tr>
+<tr>
+<td>
+
+<input type="submit" name="search" value="پیشنهاد غذا" >
+
+
+</td>
+</tr>
+</table>
 </body>
 </html>
